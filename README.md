f8081b4fa5c1b25c504ccf2fc5d5f33dc8f060aa
Pablo Balderas <fosfo.balderas@gmail.com>
2023-04-06 00:59:43 -0700

    all tasks handlers working
    - updated db queries to use request attributes in all tasks handlers
    - added README with basic info

---
+++ README.md
@@ -0,0 +1,16 @@
+# Hatch Exercise - Backend
+The backend app used for the exercise.
+
+## Requirements
+- Docker
+- Node.js v17.9.1
+
+## Installation
+`yarn install`
+
+## Running Locally
+1. You will need a .env file in the root folder, you probably received this along with the url of this repo :smiley.
+2. Run the docker-compose command to initialize the db, this can take up to 60 seconds: `docker-compose up -d`
+3. Run the webserver: `yarn dev`
+
+
--- src/handlers/tasks.js
+++ src/handlers/tasks.js
@@ -10,43 +10,95 @@ module.exports = class Tasks {
    * @param {*} next 
    */
   static async create(req, res, next) {
+    try {
 
-    console.log('Tasks.create');
-    const result = 'success';
-    res.status(200).send(result);
+      console.log(`Tasks.create - req body: ${JSON.stringify(req.body, null, 2)}`);
 
+      // lots of values we are not accepting here
+      if (!req.body?.label) {
+        console.log('Tasks.create - falsey value provided for req.body.label');
+        return res.status(400).send('Bad request');
+      }
+
+      const newRow = {
+        label: req.body.label
+      };
+      await DB.client.table('tasks').insert(newRow);
+      // const result = {
+      //   id: rows[0], // we are inserting a single newRow, so it will always be the 0 index
+      //   label: req.body.label,
+      //   done: false // default value
+      // };
+      return res.status(202).send();
+
+    } catch (err) {
+
+      console.log('Tasks.create', err);
+      return res.status(500).send('Internal server error');
+
+    }
   }
   
   /**
    * GET
-   *  - remember to prepare to read query string to be able to
-   *      filter by label later on (search feature)
    * 
    * @param {*} req 
    * @param {*} res 
    * @param {*} next 
    */
   static async read(req, res, next) {
+    try {
 
-    console.log('Tasks.read');
-    const result = [];
-    res.status(200).send(result);
+      console.log(`Tasks.read - query string: ${JSON.stringify(req.query, null, 2)}`);
 
+      // TO-DO:
+      //      - prepare to read query string to be able to
+      //        filter by label later on (search feature)
+      const rows = await DB.client.from('tasks').select();
+
+      const result = rows;
+      return res.status(200).send(result);
+
+    } catch (err) {
+
+      console.log('Tasks.read', err);
+      return res.status(500).send('Internal server error');
+
+    }
   }
 
   /**
-   * PUT
+   * PATCH
    * 
    * @param {*} req 
    * @param {*} res 
    * @param {*} next 
    */
   static async update(req, res, next) {
+    try {
 
-    console.log('Tasks.update');
-    const result = 'success';
-    res.status(200).send(result);
+      console.log(`Tasks.update - req body: ${JSON.stringify(req.body, null, 2)}`);
+      const { id, done } = req.body;
 
+      // check if invalid req
+      if (!id || !done) {
+        console.log('Tasks.update - falsey values provided for task');
+        return res.status(400).send('Bad request');
+      }
+
+      // update db
+      await DB.client.from('tasks')
+        .where({ id })
+        .update({ done });
+
+      return res.status(202).send();
+
+    } catch (err) {
+
+      console.log('Tasks.update', err);
+      return res.status(500).send('Internal server error');
+
+    }
   }
 
   /**
@@ -57,11 +109,18 @@ module.exports = class Tasks {
    * @param {*} next 
    */
   static async delete(req, res, next) {
+    try {
 
-    console.log('Tasks.delete');
-    const result = 'success';
-    res.status(200).send(result);
+      console.log('Tasks.delete');
+      await DB.client.from('tasks').del();
+      return res.status(202).send();
 
+    } catch (err) {
+
+      console.log('Tasks.delete', err);
+      return res.status(500).send('Internal server error');
+
+    }
   }
 
 };
